62d33c71dfa8d8c1c68b92572d78442526b31be6
Suvam Ghosh <suvam25@kgpian.iitkgp.ac.in>
2026-04-23 01:55:00 +0530

    style: update mobile menu styling with backdrop blur and refined background colors

--- src/section/navbar.jsx
+++ src/section/navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
                 <div className="md:hidden">
                     <button className="text-white px-6 py-2 rounded hover:bg-blue-600 hover:text-white" onClick={() => setMenu(!menu)}>{menu ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}</button>
                 </div>
-                <ul className={menu ? "absolute top-19  z-50 right-0 flex-col justify-end bg-gray-800" : "hidden md:flex space-x-4 justify-end"}>
+                <ul className={menu ? "absolute top-19  z-50 right-0 flex-col justify-end bg-gray-100/10 backdrop-blur-xl" : "hidden md:flex md:space-y-0 md:space-x-0 space-y-4 justify-end bg-light-100/80 backdrop-blur-sm"}>
                     <li className="text-blue-500 px-10 py-4 hover:bg-blue-500 hover:text-white">Home</li>
                     <li className="text-blue-500 px-10 py-4 hover:bg-blue-500 hover:text-white">Projects</li>
                     <li className="text-blue-500 px-10 py-4 hover:bg-blue-500 hover:text-white">About</li>
